173a48998ef03ef783588de088445c731b0a0f9a
Richard Doe <richard.doe@rwdit.net>
2018-09-18 12:37:01 +0100

    Style

--- spec/apps/api/boot.rb
+++ spec/apps/api/boot.rb
@@ -4,4 +4,4 @@ require 'rack'
 require 'rack/handler/puma'
 require_relative 'api_app'
 
-Rack::Handler::Puma.run(APIApp.freeze.app, :Port => 9292)
+Rack::Handler::Puma.run(APIApp.freeze.app, Port: 9292)
--- spec/apps/media/boot.rb
+++ spec/apps/media/boot.rb
@@ -4,4 +4,4 @@ require 'rack'
 require 'rack/handler/puma'
 require_relative 'media_app'
 
-Rack::Handler::Puma.run(MediaApp.freeze.app, :Port => 9393)
+Rack::Handler::Puma.run(MediaApp.freeze.app, Port: 9393)
